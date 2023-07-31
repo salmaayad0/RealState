01cd8920c223410e561358b5e99e7a1aabfd1c36
salmaayad0 <salmaayad017@gmail.com>
2023-07-31 14:46:20 +0300

    header and hero

--- realstate/src/App.js
+++ realstate/src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <>
+    <div className='app'>
+      <div>
+        <div className='white-gradient' />
+      <Header />
+      <Hero />
+      </div>
+    </div>
     </>
   );
 }
---
+++ realstate/src/components/Hero/Hero.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Hero.css";
+import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
+
+function Hero() {
+  return (
+    <section className="he-warpper">
+      <div className="paddings innerWidth flexCenter he-container">
+        {/* left side */}
+        <div className="flexColStart he-left">
+          <div className="he-title">
+            <div className="orange-circle" />
+            <h1>
+              Discover <br />
+              Most Suitable <br />
+              Property
+            </h1>
+          </div>
+
+          <div className="flexColStart he-desc">
+            <span className="secondaryText">Find a variety of properties that suit you very easilty</span>
+            <span className="secondaryText">Forget all difficulties in finding a residence for you</span>
+          </div>
+
+          <div className="flexCenter he-search">
+            <HiLocationMarker color="var(--blue)" size={25} />
+            <input type="text" />
+            <button className="button">search</button>
+          </div>
+
+          <div className="flexCenter he-stats">
+            <div className="flexColCenter he-stat">
+              <span>
+                <CountUp start={8800} end={9000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Premium Product</span>
+            </div>
+
+            <div className="flexColCenter he-stat">
+              <span>
+                <CountUp start={1950} end={2000} duration={5} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Happy Customer</span>
+            </div>
+
+            <div className="flexColCenter he-stat">
+              <span>
+                <CountUp start={5} end={28} duration={6} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Awards Winning</span>
+            </div>
+          </div>
+        </div>
+
+        {/* right side image side */}
+        <div className="flexCenter he-right">
+          <div className="img-container">
+            <img src="./media/hero-image.png" alt="home" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
